01569c55f74905e321a277daec93e42c4d7d8a75
July Manova <jmanova83@gmail.com>
2017-06-25 22:18:51 +0300

    04-05/08 solved?

---
+++ WriteToFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteToFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = File.ReadAllText("sample_text.txt");
+
+			string output = string.Join("", text.Replace(".", "").Replace(",", "").Replace("!", "").Replace("?", "").Replace(":", ""));
+			File.WriteAllText("output.txt", output);
+		}
+	}
+}
