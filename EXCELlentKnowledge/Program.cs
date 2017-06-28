3a790011f6d2cac0020278cf5288e35909f8566a
Stranger <jmanova83@gmail.com>
2017-06-28 23:12:13 +0300

    excel not solved

---
+++ EXCELlentKnowledge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCELlentKnowledge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var neededResult = "ZIP | Sales | Name | Year | Value |";
+			neededResult += "2121 | 456 | Jane | 2011 | 84219 |";
+			neededResult += "2092 | 789 | Ashish | 2012 | 28322 |";
+			neededResult += "2128 | 456 | Jane | 2013 | 81878 |";
+			neededResult += "2073 | 123 | John | 2011 | 44491 |";
+		}
+	}
+}
